56ceb4e8bcf9d179cf852f53f30f8480e0cd8fae
wooni <sjchu91@gmail.com>
2023-03-21 09:01:54 +0900

    [WOONI-1] divide module.

--- libs/adapter-persistence/src/main/java/sample/wooni/blog/config/BlogJpaConfig.java
+++ libs/adapter-persistence/src/main/java/sample/wooni/blog/config/BlogJpaConfig.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import sample.wooni.blog.RepositoryModule;
 import sample.wooni.blog.entity.EntityModule;
 
@@ -19,5 +22,17 @@ public class BlogJpaConfig {
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(this.entityManager);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager();
+    }
+
+    @Bean
+    public TransactionTemplate readOperation(PlatformTransactionManager transactionManager) {
+        var transactionTemplate = new TransactionTemplate(transactionManager);
+        transactionTemplate.setReadOnly(true);
+        return transactionTemplate;
     }
 }
--- libs/application/src/main/java/sample/wooni/blog/service/statistics/BlogStatisticsServiceImpl.java
+++ libs/application/src/main/java/sample/wooni/blog/service/statistics/BlogStatisticsServiceImpl.java
@@ -1,6 +1,7 @@
 package sample.wooni.blog.service.statistics;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionOperations;
 import sample.wooni.blog.service.output.blog.statistics.BlogStatisticsOutput;
 import sample.wooni.blog.service.search.dto.BlogSearchType;
 import sample.wooni.blog.service.statistics.converter.BlogStatisticsDtoConverter;
@@ -12,14 +13,17 @@ import java.util.List;
 public class BlogStatisticsServiceImpl implements BlogStatisticsService {
 
     private final BlogStatisticsOutput blogStatisticsOutput;
+    private final TransactionOperations readOperation;
 
-    public BlogStatisticsServiceImpl(BlogStatisticsOutput blogStatisticsOutput) {
+    public BlogStatisticsServiceImpl(BlogStatisticsOutput blogStatisticsOutput,
+                                     TransactionOperations readOperation) {
         this.blogStatisticsOutput = blogStatisticsOutput;
+        this.readOperation = readOperation;
     }
 
     @Override
     public List<BlogStatisticsDto> top10SearchKeywords(BlogSearchType type) {
-        var response = blogStatisticsOutput.fetchTop10Keyword(type);
+        var response = readOperation.execute( status -> blogStatisticsOutput.fetchTop10Keyword(type));
         return BlogStatisticsDtoConverter.convert(response);
     }
 }
